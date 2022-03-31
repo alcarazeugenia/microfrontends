e36aef3a1d50219eb65fb6fc7745a508a8b8af64
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-03-31 14:12:50 -0300

    Updated text to redeploy

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
 	return (
 		<>
-			<h1>Hey!</h1>
+			<h1>Hey! Now doing CI-CD</h1>
 			<hr />
 			<MarketingApp />
 		</>
