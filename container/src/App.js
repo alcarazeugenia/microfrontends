0d4a77610974d71b4fa585464400af3b57085b7d
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-03-31 10:32:48 -0300

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+	return (
+		<>
+			<h1>Hey!</h1>
+			<hr />
+			<MarketingApp />
+		</>
+	);
+};
