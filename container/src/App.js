b590a471122e40346678fd66bbec07142652661f
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-04-01 12:27:58 -0300

    Added vue dashboard

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import {
 	StylesProvider,
 	createGenerateClassName,
 } from "@material-ui/core/styles";
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: "co",
@@ -16,6 +17,12 @@ export default () => {
 		<BrowserRouter>
 			<StylesProvider generateClassName={generateClassName}>
 				<Header />
+				<Suspense fallback="...loading">
+					<Switch>
+						<Route path="/dashboard" component={DashboardLazy} />
+						<Route path="/" component={MarketingApp} />
+					</Switch>
+				</Suspense>
 				<MarketingApp />
 			</StylesProvider>
 		</BrowserRouter>
